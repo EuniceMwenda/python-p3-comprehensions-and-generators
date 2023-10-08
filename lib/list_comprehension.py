5a8f043cc7cb4c7c5d49c22846708c3441734564
Eunice Mwenda <eunice.mwenda@student.moringaschool.com>
2023-10-08 20:28:08 +0300

    Comprehensions and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+
+    return [x for x in num_list if x % 2 == 0]
+
+
+result = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(result)  # Output: [0, 8]
+pass
 
 def make_exclamation(sentence_list):
-    pass
+   
+    return [sentence + '!' for sentence in sentence_list]
+
+
+result = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(result)  
+
+pass
